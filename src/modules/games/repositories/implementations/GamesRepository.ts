9a58d5d4ca3bde775e8a56491c9a426e5dfc3411
pablorodrigo <pablo.silva.rodrigo@gmail.com>
2021-07-06 14:09:42 -0400

    Completed challenge

--- src/modules/games/repositories/implementations/GamesRepository.ts
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -1,9 +1,9 @@
-import { getRepository, Repository } from 'typeorm';
+import {getRepository, Repository} from 'typeorm';
 
-import { User } from '../../../users/entities/User';
-import { Game } from '../../entities/Game';
+import {User} from '../../../users/entities/User';
+import {Game} from '../../entities/Game';
 
-import { IGamesRepository } from '../IGamesRepository';
+import {IGamesRepository} from '../IGamesRepository';
 
 export class GamesRepository implements IGamesRepository {
   private repository: Repository<Game>;
@@ -12,19 +12,21 @@ export class GamesRepository implements IGamesRepository {
     this.repository = getRepository(Game);
   }
 
-  async findByTitleContaining(param: string): Promise<Game[]> {
-    return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+  async findByTitleContaining(title: string): Promise<Game[]> {
+    return await this.repository
+      .createQueryBuilder("games")
+      .where(`LOWER(games.title) like LOWER(:title)`,{title: `%${title}%`}).getMany()
+    // Complete usando query builder
   }
 
   async countAllGames(): Promise<[{ count: string }]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT COUNT(*) FROM queries_challenge.public.games`); 
+    // Complete usando raw query
   }
 
   async findUsersByGameId(id: string): Promise<User[]> {
     return this.repository
-      .createQueryBuilder()
-      // Complete usando query builder
+      .createQueryBuilder("games").relation(Game,"users").of(id).loadMany()
+    // Complete usando query builder
   }
 }
--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -15,16 +15,19 @@ export class UsersRepository implements IUsersRepository {
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
     // Complete usando ORM
+    const user = await this.repository.findOneOrFail(user_id,{relations: ['games']});
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query("SELECT * FROM queries_challenge.public.users ORDER BY first_name"); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM queries_challenge.public.users WHERE LOWER(first_name)=LOWER($1) AND LOWER(last_name)=LOWER($2)`,[first_name,last_name]); // Complete usando raw query
   }
 }
